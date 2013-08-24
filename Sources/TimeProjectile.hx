aaa7e0783a13bf08e85449c80eac49d89fdf1db4
Ingo Jakobs <ingo.jakobs@stud.tu-darmstadt.de>
2013-08-24 17:14:11 +0200

    TimeWeapon works now

--- Sources/TimeProjectile.hx
+++ Sources/TimeProjectile.hx
@@ -1,7 +1,9 @@
 package ;
 
+import kha.Direction;
 import kha.Image;
 import kha.Painter;
+import kha.Scene;
 import kha.Sprite;
 
 class TimeProjectile extends Sprite {
@@ -13,5 +15,16 @@ class TimeProjectile extends Sprite {
 	{
 		painter.setColor( kha.Color.fromBytes(0, 255, 0) );
 		painter.fillRect( x, y, width, height );
+	}
+	
+	override public function hitFrom( dir:Direction ) : Void {
+		Scene.the.removeProjectile(this);
+	}
+	
+	override public function hit(sprite:Sprite) : Void {
+		if ( Std.is( sprite, TimeTravelSprite ) ) {
+			cast( sprite, TimeTravelSprite ).timeLeap();
+		}
+		Scene.the.removeProjectile(this);
 	}
 }

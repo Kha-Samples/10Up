0384f6b8107237f96c2f7f1708c26f13356d3e68
Robert Konrad <robert@ktx-software.com>
2013-08-26 16:47:17 +0200

    Fixed loading of remaining images.

--- Sources/Gatter.hx
+++ Sources/Gatter.hx
@@ -5,8 +5,9 @@ import kha.Sprite;
 
 class Gatter extends Sprite {
 	public function new(x: Float, y: Float) {
-		super(Loader.the.getImage("gatter"), 10, 10);
+		super(Loader.the.getImage("gatter"), 32 * 2, 6 * 2);
 		this.x = x;
 		this.y = y;
+		accy = 0;
 	}	
 }
